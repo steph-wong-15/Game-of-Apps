c2138b4832cde5e2f47580c7e5a2c24c22735479
Halakseka J <hjegathe@sfu.ca>
2021-03-27 18:56:31 -0700

    testing

--- connection.py
+++ connection.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 
+ 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'fdsafdsa'
-
+ 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'goa'
+ 
 mysql = MySQL(app)
 
-
-@app.route('/')
-def home():
+@app.route("/")
+def searchPage():
     return render_template("home.html")
 
 
-@app.route('/students')
-def students():
+
+@app.route("/students", methods = ['GET','POST'])
+def search():
     id = request.form['id']
-    cursor = mysql.connection.cursor()
+    cursor = mysql.get_db().cursor()
     string = "SELECT * FROM goaUser WHERE UserID = %s ;"
     cursor.execute(string, (id))
     fetchdata = cursor.fetchall()
@@ -27,5 +28,7 @@ def students():
     return render_template("students.html", data = fetchdata)
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
+ 
