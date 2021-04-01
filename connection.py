04259f03bdccd880b9b2f6817fdbcf39cfe0ca46
Annie Yao <50132471+qyyao@users.noreply.github.com>
2021-04-01 15:34:53 -0700

    working lesson slides

--- connection.py
+++ connection.py
@@ -11,20 +11,42 @@ app.config['MYSQL_DATABASE_DB'] = 'goa'
 mysql = MySQL(app)
 
 
+
 @app.route("/")
 def homePage():
     return render_template("home.html")
 
+
+@app.route('/lessons')
+def lessons():
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM lesson ORDER BY weekNumber"
+    cursor.execute(string)
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    return render_template("lessons.html", lessons = fetchdata)
+
+@app.route('/lessonDetails/<string:ID>')
+def lessonDetails(ID):
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM lesson WHERE LessonID = %s ;"
+    cursor.execute(string, ID)
+    lessonData = cursor.fetchall()
+    cursor.close()
+    return render_template("lessonDetails.html", lessonData = lessonData)
+
+
 @app.route('/students')
 def students():
     return render_template("students.html")
+
 
 @app.route('/student_profile', methods=['GET', 'POST'])
 def studentProfile():
     id = request.form['id']
     cursor = mysql.get_db().cursor()
     string = "SELECT * FROM goaUser WHERE UserID = %s ;"
-    cursor.execute(string, (id))
+    cursor.execute(string, id)
     fetchdata = cursor.fetchall()
     cursor.close()
     return render_template("student_profile.html", data=fetchdata)
