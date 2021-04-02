b3dd284071858ef423e31471aa504b8c43e6f70d
hj <80874689+h375@users.noreply.github.com>
2021-04-01 19:33:34 -0700

    added list of students

--- connection.py
+++ connection.py
@@ -15,19 +15,30 @@ mysql = MySQL(app)
 def homePage():
     return render_template("home.html")
 
-@app.route('/students')
-def students():
-    return render_template("students.html")
+# @app.route('/students')
+# def students():
+#     return render_template("students.html")
+
+
+@app.route('/students', methods=['GET', 'POST'])
+def studentTable():
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM goaUser")
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    #print(fetchdata)
+    return render_template("students.html", student =fetchdata)
 
 @app.route('/student_profile', methods=['GET', 'POST'])
-def studentProfile():
+def studentSearch():
     id = request.form['id']
     cursor = mysql.get_db().cursor()
     string = "SELECT * FROM goaUser WHERE UserID = %s ;"
     cursor.execute(string, (id))
     fetchdata = cursor.fetchall()
     cursor.close()
-    return render_template("student_profile.html", data=fetchdata)
+    return render_template("student_profile.html", student=fetchdata)
+
 
 
 if __name__ == "__main__":
