26d80b9a89c9e3487293947a6ac11c070c92d322
adwjsnpl1252 <adwjsnpl1252@gmail.com>
2021-04-04 11:32:29 +0900

    update

--- connection.py
+++ connection.py
@@ -3,21 +3,15 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Atjdwkd0124@'
-app.config['MYSQL_DATABASE_DB'] = 'FLASK'
+
 
 mysql = MySQL(app)
 
 
-@app.route("/")
-def searchPage():
-    return render_template("home.html")
 
-@app.route('/students')
-def students():
-    return render_template("students.html")
+
+
+
 
 @app.route('/events')
 def events():
@@ -47,15 +41,7 @@ def eventDetails():
     cursor.close()
     return render_template("eventDetails.html",eventData=fetchdata)
 
-@app.route('/student_profile', methods=['GET', 'POST'])
-def studentProfile():
-    id = request.form['id']
-    cursor = mysql.get_db().cursor()
-    string = "SELECT * FROM goaUser WHERE UserID = %s ;"
-    cursor.execute(string, (id))
-    fetchdata = cursor.fetchall()
-    cursor.close()
-    return render_template("student_profile.html", data=fetchdata)
+
 
 @app.route('/suggestionDetails',methods=['GET','POST'])
 def suggestionDetails():
@@ -66,6 +52,83 @@ def suggestionDetails():
     fetchdata = cursor.fetchall()
     cursor.close()
     return render_template("suggestionDetails.html",suggestionData=fetchdata)
+
+
+@app.route("/")
+def homePage():
+    return render_template("home.html")
+
+
+@app.route('/lessons')
+def lessons():
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM lesson ORDER BY weekNumber"
+    cursor.execute(string)
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    return render_template("lessons.html", lessons = fetchdata)
+
+
+@app.route('/lessonDetails/<string:ID>')
+def lessonDetails(ID):
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM lesson WHERE LessonID = %s ;"
+    cursor.execute(string, ID)
+    lessonData = cursor.fetchall()
+    cursor.close()
+    return render_template("lessonDetails.html", lessonData = lessonData)
+
+
+@app.route('/assignment/<string:lessonID>')
+def assignment(lessonID):
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM assignment WHERE LessonID = %s ;"
+    cursor.execute(string, lessonID)
+    assignmentData = cursor.fetchall()
+    cursor.close()
+    return render_template("assignment.html", assignmentData = assignmentData)
+
+
+@app.route('/challenge/<string:lessonID>')
+def challenge(lessonID):
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM question WHERE ChallengeID IN (SELECT challengeID FROM challenge WHERE LessonID = %s);"
+    cursor.execute(string, lessonID)
+    questions = cursor.fetchall()
+    cursor.close()
+    return render_template("challenge.html", questions = questions)
+
+
+
+@app.route('/students', methods=['GET', 'POST'])
+def studentTable():
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM goaUser")
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    #print(fetchdata)
+    return render_template("students.html", student =fetchdata)
+
+
+@app.route('/student_profile', methods=['GET', 'POST'])
+def studentSearch():
+    id = request.form['id']
+    cursor = mysql.get_db().cursor()
+    string = "SELECT * FROM goaUser WHERE UserID = %s ;"
+    cursor.execute(string, id)
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    return render_template("student_profile.html", student=fetchdata)
+
+
+@app.route('/teams',  methods=['GET', 'POST'])
+def teams():
+	cursor = mysql.get_db().cursor()
+	string = "SELECT * FROM team ORDER BY VotesReceived DESC;"
+	cursor.execute(string)
+	fetchdata = cursor.fetchall()
+	cursor.close()
+	return render_template("teams.html", teams=fetchdata)
 
 
 if __name__ == "__main__":
